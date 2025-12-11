d483d2795f3594c10a1c3dbf86fd71c820fb64e4
Shande Alvan <ashande75@gmail.com>
2025-12-11 15:56:19 -0500

    --Se agregó lógica de enviar mensaje

--- schemas/mensajes_schema.py
+++ schemas/mensajes_schema.py
@@ -2,11 +2,8 @@ from pydantic import BaseModel
 from sqlalchemy import JSON
 
 class MensajesSchema(BaseModel):
-    id_contacto:int
     wa_id:str
     nombre:str
-    id:int
-    id_contact:int
     id_usuario:int
     direction:str
     waba_message_id:str
--- services/contactos_service.py
+++ services/contactos_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from models.contactos import Contactos
 from models.mensajes import Mensajes
 from schemas.mensajes_schema import MensajesSchema
+from fastapi.responses import JSONResponse
 
 def save_mensaje(db:Session,data:MensajesSchema):
     contactos=db.query(Contactos).filter(Contactos.wa_id==data.wa_id).first()
@@ -14,5 +15,24 @@ def save_mensaje(db:Session,data:MensajesSchema):
         db.add(contactos)
         db.commit()
         db.refresh(contactos)
-    return "elegante"    
+        
+    mensaje=Mensajes(
+        id_contact=contactos.id,
+        id_usuario=data.id_usuario,
+        direction=data.direction,
+        waba_message_id=data.waba_message_id,
+        message_type=data.message_type,
+        text_content=data.text_content,
+        raw_json=data.raw_json,
+        timestamp=data.timestamp,
+        estado='A'
+    )
+    db.add(mensaje)
+    db.commit()
+    db.refresh(mensaje)
+    
+    return JSONResponse(content={
+        "message":"El mensaje fue enviado correctamente",
+        "status":200
+    })    
     
